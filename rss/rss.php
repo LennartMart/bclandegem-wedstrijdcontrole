e19e7681c019f75f7984f2bda35e55d6a2c86d0b
LennartMart <LennartMart@users.noreply.github.com>
2020-09-29 13:37:21 +0200

    Fix RSS links

--- rss/rss.php
+++ rss/rss.php
@@ -1,14 +1,14 @@
 <?php
 ///////
 // Very simple script to clean the blankline from an RSS FEED.
-$url = "http://www.bclandegem.be/?format=feed&type=rss";
+$url = "https://www.bclandegem.be/?format=feed&type=rss";
  
 if(isset($_GET['type'])){
    if(($_GET['type']) == "intraclub"){
-      $url = "http://bclandegem.be/club/alle-artikels/club/25-intraclub?format=feed&type=rss";
+      $url = "https://www.bclandegem.be/club/alle-artikels/club/intraclub?format=feed&type=rss";
    }
    else if(($_GET['type']) == "competitie") {
-	   $url = "http://bclandegem.be/competitie/competitieverslagen?format=feed&type=rss";
+	   $url = "https://www.bclandegem.be/competitie/competitieverslagen?format=feed&type=rss";
    }
 	   
 }
