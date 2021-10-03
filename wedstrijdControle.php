0568a1642b3a85d8954762f878d6dd396a9926cc
Lennart Martens <lmartens@metanous.be>
2021-10-03 19:56:00 +0200

    WedstrijControle: revamp. - Upgrade PHPMailer - Rekening houden met forfait - Calculatie tijd tot boete

--- wedstrijdControle.php
+++ wedstrijdControle.php
@@ -1,88 +1,96 @@
 <?php
+
 /**
  * User: Lennart
  * Date: 11/11/14
  * Time: 22:51
  */
+
+use PHPMailer\PHPMailer\PHPMailer;
+
 require_once("simple_html_dom.php");
 require_once("config.php");
-require 'PHPMailer/PHPMailerAutoload.php';
 
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+// DEBUG
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+$aantalUrenOmUitslagInTeVullen = 36;
+$aantalUrenOmTeBevestigen = 72;
 
 $matchenUitUrls = parseBadmintonVlaanderenUrls(unserialize(BADMINTONVLAANDEREN_URLS));
 $ploegen = unserialize(PLOEGEN);
 
-$huidigTijdstip = new DateTime("now",new DateTimeZone('Europe/Brussels'));
+$huidigTijdstip = new DateTime("now", new DateTimeZone('Europe/Brussels'));
 $fouten = array();
 
-foreach ($matchenUitUrls as $match)
-{
-	if($huidigTijdstip > $match->tijdstip)
-	{
-		//$ploegNaam thuisploeg - check of de uitslag is ingevuld
-		if (strpos($match->thuisPloeg,PLOEGNAAM) !== false)
-		{
-			//Thuisploeg
-			if(empty($match->uitslag))
-			{
-				$ploeg = vindPloeg($ploegen, $match->thuisPloeg);
-				$ploeg->fouten[] ="<li> <a href='" .  $match->link ."'>" . $match->getTitelMatch() . "</a> is nog niet ingevuld.</li>";
-				
-			}
-		}
-		else
-		{
-			//Uitploeg
-			if(!empty($match->uitslag) && $match->goedgekeurd == false)
-			{
-				$ploeg = vindPloeg($ploegen, $match->uitPloeg);
-				$ploeg->fouten[] = "<li> <a href='" .  $match->link ."'>" . $match->getTitelMatch() . "</a> is nog niet bevestigd.</li>";
-			}
-		}
-	}
+foreach ($matchenUitUrls as $match) {
+    if ($huidigTijdstip > $match->tijdstip) {
+        //$ploegNaam thuisploeg - check of de uitslag is ingevuld
+        if (strpos($match->thuisPloeg, PLOEGNAAM) !== false) {
+            //Thuisploeg
+            if (empty($match->uitslag)) {
+                $tijdOver = berekenTijdTotBoete($huidigTijdstip, $match->tijdstip, $aantalUrenOmUitslagInTeVullen);
+                $ploeg = vindPloeg($ploegen, $match->thuisPloeg);
+                $ploeg->fouten[] = "<li> <a href='" .  $match->link . "'>" .
+                    $match->getTitelMatch() . "</a> is nog niet ingevuld. Je hebt nog " . $tijdOver . " uren om de uitslag in te vullen!";
+            }
+        } else {
+            //Uitploeg
+            if (!empty($match->uitslag) && $match->goedgekeurd == false && $match->forfait == false) {
+                $tijdOver = berekenTijdTotBoete($huidigTijdstip, $match->tijdstip, $aantalUrenOmTeBevestigen);
+                $ploeg = vindPloeg($ploegen, $match->uitPloeg);
+                $ploeg->fouten[] = "<li> <a href='" .  $match->link . "'>" . $match->getTitelMatch()
+                    . "</a> is nog niet bevestigd.</li> . Je hebt nog " . $tijdOver . " uren om de uitslag te bevestigen!";
+            }
+        }
+    }
 }
-foreach($ploegen as $ploeg) {
-	if(!empty($ploeg->fouten))
-	{
-		sendMail($ploeg);
-	}
-	else
-	{
-		echo "Geen fouten gevonden voor " . $ploeg->ploegNaam . " <br/>";
-	}
+foreach ($ploegen as $ploeg) {
+    if (!empty($ploeg->fouten)) {
+        sendMail($ploeg);
+    } else {
+        echo "Geen fouten gevonden voor " . $ploeg->ploegNaam . " <br/>";
+    }
 }
-	
-	
 
-	
+function berekenTijdTotBoete($huidigTijdstip, $tijdstipMatch, $officiëleTijd)
+{
+    $tijdVerschil = $huidigTijdstip->diff($tijdstipMatch);
+
+    $urenVerschil = $tijdVerschil->h;
+    $urenVerschil = $urenVerschil + ($tijdVerschil->days * 24);
+    return $officiëleTijd - $urenVerschil;
+}
 function parseBadmintonVlaanderenUrls($badmintonUrls)
 {
     $lijstMatchen = array();
-    foreach($badmintonUrls as $url)
-    {
+    foreach ($badmintonUrls as $url) {
         //haal de html op
         $html = file_get_html($url);
         //Haal alle rijen op in de wedstrijden tabel
         $rijen = $html->find('table[class=ruler] tr');
-        
-		if(empty($rijen))
-		{
-			//Tijdelijk geen e-mail sturen - tabellen zijn gewoon leeg
-			echo "Geen rijen gevonden op ".  $url . "<br/>";
-			//sendMail(array("<li>De tabelstructuur van Badminton Vlaanderen kon niet worden ingelezen. Mogelijks is de website gewijzigd, of zijn er geen matchen meer beschikbaar!</li>"));
-		}
+
+        if (empty($rijen)) {
+            //Tijdelijk geen e-mail sturen - tabellen zijn gewoon leeg
+            echo "Geen rijen gevonden op " .  $url . "<br/>";
+            //sendMail(array("<li>De tabelstructuur van Badminton Vlaanderen kon niet worden ingelezen. Mogelijks is de website gewijzigd, of zijn er geen matchen meer beschikbaar!</li>"));
+        }
         //We hebben de eerste rij niet nodig - vreemde bug html_dom die tbody niet herkent als selector
         array_shift($rijen);
-        foreach($rijen as $rij)
-        {
-            $match = new Match();
+        foreach ($rijen as $rij) {
+            $match = new Wedstrijd();
             $celnummer = 0;
-            foreach($rij->find('td') as $cel)
-            {
-                switch($celnummer){
+            foreach ($rij->find('td') as $cel) {
+                switch ($celnummer) {
                     case 1: //TIJDSTIP
                         //Verwijder dagaanduiding, niet nodig
-                        $tijdstip = substr($cel->innertext(),3);
+                        $tijdstip = substr($cel->innertext(), 3);
                         $tijdstip = strip_tags($tijdstip);
                         $match->tijdstip = DateTime::createFromFormat("d/m/Y H:i", $tijdstip, new DateTimeZone('Europe/Brussels'));
                         break;
@@ -101,11 +109,16 @@ function parseBadmintonVlaanderenUrls($badmintonUrls)
                         break;
                     case 9: //UITSLAG
                         $match->uitslag = $cel->innertext();
+                        if (strpos($match->uitslag, "Niet gespeeld") !== false) {
+                            $match->forfait = true;
+                        } else {
+                            $match->forfait = false;
+                        }
                         break;
                     case 10: //GECHECKT
-                        if(($cel->find('img', 0))) {
+                        if (($cel->find('img', 0))) {
                             $match->goedgekeurd = true;
-                        }else{
+                        } else {
                             $match->goedgekeurd = false;
                         }
                         break;
@@ -124,7 +137,7 @@ function sendMail($ploeg)
 {
     $mail = new PHPMailer;
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
+    $mail->Host = 'mail.bclandegem.be';                       // Specify main and backup server
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = EMAIL_USERNAME;                   // SMTP username
     $mail->Password = EMAIL_PASSWORD;               // SMTP password
@@ -132,10 +145,10 @@ function sendMail($ploeg)
     $mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
     $mail->setFrom(EMAIL_USERNAME, 'Wedstrijdcontrole competitie');     //Set who the message is to be sent from
 
-	$mail->addAddress($ploeg->email);
+    $mail->addAddress($ploeg->email);
     $ontvangers = unserialize(EMAIL_ONTVANGERS);
     foreach ($ontvangers as $ontvanger) {
-		$mail->addAddress($ontvanger);
+        $mail->addAddress($ontvanger);
     }
     $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
     $mail->isHTML(true);                                  // Set email format to HTML
@@ -150,8 +163,8 @@ function sendMail($ploeg)
     $mail->Body    = $bodyText;
     $mail->AltBody = 'Er zijn enkele waarschuwingen. Bezoek www.badmintonvlaanderen.be en controleer de matchen.';
 
-    if(!$mail->send()) {
-        echo 'Message could not be sent.';
+    if (!$mail->send()) {
+        echo 'Message could not be sent. <br/>';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
         exit;
     }
@@ -159,16 +172,17 @@ function sendMail($ploeg)
     echo 'Bericht goed verzonden! <br/>';
 }
 
-function vindPloeg($ploegen, $ploegNaam){
-	foreach($ploegen as $ploeg) {
-		if ($ploegNaam == $ploeg->ploegNaam) {
-			return $ploeg;
-		}	
-	}
-	return null;
+function vindPloeg($ploegen, $ploegNaam)
+{
+    foreach ($ploegen as $ploeg) {
+        if ($ploegNaam == $ploeg->ploegNaam) {
+            return $ploeg;
+        }
+    }
+    return null;
 }
 
-class Match
+class Wedstrijd
 {
     public $tijdstip;
     public $thuisPloeg;
@@ -176,12 +190,10 @@ class Match
     public $goedgekeurd;
     public $uitslag;
     public $link;
+    public $forfait;
 
     function getTitelMatch()
     {
         return $this->thuisPloeg . ' - ' . $this->uitPloeg;
     }
 }
-
-
-
